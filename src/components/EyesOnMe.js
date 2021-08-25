f6fd27f014e82130c2727f03059339ed261dd91a
Isaac <devisaacalejandro@gmail.com>
2021-08-25 02:32:28 -0400

    commit tree f0ea7e0ce692ca9e17196a00bf57fef9b727e50d

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React, {Component} from 'react'
+
+
+export default class EyesOnMe extends Component{
+    render(){
+        
+        handleFocus = () => console.log('Good!')
+
+        handleBlur = () => console.log('Hey! Eyes on me!')
+        return(
+            <div>
+                <button
+                onFocus={this.handleFocus}
+                onBlur={this.handleBlur}
+                >
+                   soemthing 
+                </button>
+            </div>
+        )
+    }
+
+}
